bbd0ee1c22fb77827b3c77a8d8a13d465955e6e4
hgx <Lichaobivip@163.com>
2019-12-16 17:32:52 +0800

    修改

--- jd/src/main.js
+++ jd/src/main.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
 import VueRouter from "vue-router"
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import VueAwesomeSwiper from 'vue-awesome-swiper'
-import 'swiper/dist/css/swiper.css'
+
+
+
+
 import router from"./routers/index.js"
 import App from './App.vue'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
-Vue.use(ElementUI);
-Vue.use(VueAwesomeSwiper)
+
+
 new Vue({
   render: h => h(App),
   router:router,
